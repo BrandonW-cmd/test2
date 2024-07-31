419e12434c404adaa3ca8502f168f3bfc3299b18
BrandonW-cmd <809bsw@gmail.com>
2024-07-31 16:38:56 +0000

    js examples

---
+++ script.js
@@ -0,0 +1,27 @@
+let myName = "Brandon"
+let myMessage = "Good morning,"
+myMessage = myMessage + myName
+
+//console.log(myMessage + myName)
+
+//console.log(10-7)
+//console.log(2*5)
+//console.log(18/6)
+//console.log(8**2)
+
+/
+
+
+//true of fale statements
+let isSunny = false
+//console.log(isSunny)
+
+// ! make it opposite false to true true to false
+//console.log(!isSunny)
+
+let quantity = "12"
+console.log(typeof quantity)
+
+quantity = Number(quantity)
+console.log(typeof quantity);
+//helps the computer find out what type of data it is
